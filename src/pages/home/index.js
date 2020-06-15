f4e9787ec1c31f0b54b39a0c651b818c3a6dc2f8
Bruno José <46513538+brunozn@users.noreply.github.com>
2020-06-15 19:24:19 -0300

    Criando a page home

---
+++ src/pages/home/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+import './style.css';
+
+//import logo from '../../assets/logo.png'; <img src={logo} classNameNameNameName="img-responsive" alt="">
+export default function Home() {
+
+    return (
+<div className="container-dash">
+
+  <div className="row">
+    <div className="col">
+      1 de 2
+    </div>
+    <div className="col">
+      2 de 2
+    </div>
+  </div>
+  <div className="row">
+    <div className="col">
+      1 de 3
+    </div>
+    <div className="col">
+      2 de 3
+    </div>
+    <div className="col">
+      3 de 3
+    </div>
+  </div>
+</div>
+
+
+    
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import New from './pages/New';
+import Home from './pages/home';
+
+import Register from './pages/Register/index';
 
 export default function Routes(){
     return (
@@ -12,6 +15,8 @@ export default function Routes(){
             <Route path="/" exact component={Login}/>
             <Route path="/dashboard" component={Dashboard}/>
             <Route path="/new" component={New}/>
+            <Route path="/home" component={Home} />
+            <Route path="/register" component={Register} />
         </Switch>
         </BrowserRouter>
     );
