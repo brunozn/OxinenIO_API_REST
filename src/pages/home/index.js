0b06d34d08efe46e106f3c4c3f8d5788b341a931
Renan Santana <renan.nascimento.santana@hotmail.com>
2020-06-20 20:14:22 -0300

    estilizaca_home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,30 +6,31 @@ export default function Home() {
   
   return (
     <>
-    <div className="container my-5">
+    <div className="container my-5 fora">
 
       <div className="row align-items-center my-5">
 
 
-      <div className="col-lg-5 imge">
-          <img src={homeImage} alt="home" />  
-          <h4> Mão na Massa</h4>
+      <div className="col-lg imge">
+          <p className="logo one">Seja bem vindo ao<br></br><span id="SPAN1">Mão na Massa</span>!</p>
+          <p className="logo two">A nossa motivação é te <span>atender bem!</span>
+          </p>
         </div>
 
         <div className="col-lg-7">
           <h2 className="home-h2">Tem um serviço e gostaria de encontrar alguém ? </h2>
           <h4 className="font-weight-light">Anuncie sua procura aqui e encontre trabalhadores proximos de você</h4>
-          <button className="btn but2" to ="/register">Cadastra-se</button>
+          <button className="btn but2" to ="/register">Cadastre-se</button>
         </div>
 
 
       </div>
-
-      <div className="card text-white bg-secondary my-10 py-5 text-center">
-        <div className="card-body">
-          <p className="text-white m-0">Ajudamos as pessoas </p>
-        </div>
+      <hr></hr>
+      <div class="col s12 m7">
+              <p id="oxigenio">OxigenIO</p>
+              <p id="arr">Todos os direitos reservados</p>
       </div>
+      
     </div>
   </>
   );
